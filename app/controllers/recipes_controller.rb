3f26e1a97c223051e865ec14d1a265423be521a7
souad988 <souadelmansouri88@gmail.com>
2023-10-11 20:12:19 +0100

    Add public recipe view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[show edit update destroy add_ingredient destroy_ingredient]
   # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.all
@@ -10,5 +11,24 @@ class RecipesController < ApplicationController
   # GET /recipes/new
   def new
     @recipe = Recipe.new
+  end
+  def public_recipes
+    @recipes = Recipe.where(public: true)
+  end
+
+  def add_ingredient
+    @food = Food.find(params[:food_id])
+    @recipe.food << @food
+  end
+
+  def destroy_ingredient
+    @food = Food.find(params[:food_id])
+    @recipe.food.delete(@food)
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :recipes
+  root 'recipes#public_recipes'
+  resources :recipes do
+    member do
+      post 'add_ingredient' # Use 'get' or 'post' as needed
+      post 'destroy_ingredient'
+    end
+  end
   resources :foods
-  resources :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
