e5bd1b3cb614e052f95fba5acde113502f2c6eb9
souad988 <souadelmansouri88@gmail.com>
2023-10-11 09:46:19 +0100

    Remove role abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,15 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new
-
-    if user.role == 'admin'
-      can :manage, :all
-    else
-      can :read, :all
-      can :delete, Food, user_id: user.id
-      can :delete, Recipe, user_id: user.id
-      can :create, Food
-      can :create, Recipe
-    end
+    can :read, :all
+    can :delete, Food, user_id: user.id
+    can :delete, Recipe, user_id: user.id
+    can :create, Food
+    can :create, Recipe
   end
 end
