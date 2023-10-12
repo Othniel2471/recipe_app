cbcbc9c175b2a4f228a26e01b870e71fa23a4e3a
souad988 <souadelmansouri88@gmail.com>
2023-10-12 13:41:20 +0100

    Add many to many link between food and recipe to access recipe.foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,5 @@
 class Food < ApplicationRecord
   belongs_to :user
-  has_many :food_recipes, through: :food_recipes, class_name: 'Recipe', foreign_key: 'recipe_id'
-
-  accepts_nested_attributes_for :food_recipes
+  has_many :food_recipes
+  has_many :recipes, through: :food_recipes, class_name: 'Recipe', foreign_key: 'recipe_id'
 end
