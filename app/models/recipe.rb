4de36dc0c6a34ee4e8079b3f03b80e41230f9f79
othniel2471 <othnielomaidu@gmail.com>
2023-10-13 18:45:36 +0100

    updated food value and delete functionalities for recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,6 +6,6 @@ class Recipe < ApplicationRecord
   validates :description, presence: true
 
   belongs_to :user
-  has_many :food_recipes
+  has_many :food_recipes, dependent: :destroy
   has_many :foods, through: :food_recipes, class_name: 'Food', foreign_key: 'food_id'
 end
